3303ae65bc13070ee2d48670dc4d6222e3f61ff7
sandeep gill <sandeep.gill@grootsoftwares.com>
2026-06-08 20:30:06 +0530

    fixing swagger

--- src/main.ts
+++ src/main.ts
@@ -33,7 +33,17 @@ async function bootstrap() {
 
   const rawDocument = SwaggerModule.createDocument(app, swaggerConfig);
   const document = cleanupOpenApiDoc(rawDocument);
-  SwaggerModule.setup('docs', app, document);
+  SwaggerModule.setup('docs', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+    customCssUrl:
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.11.0/swagger-ui.min.css',
+    customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.11.0/swagger-ui-bundle.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.11.0/swagger-ui-standalone-preset.js',
+    ],
+  });
 
   await app.listen(port);
 }
